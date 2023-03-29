3b6f9e4e2674e0dba8f0948f7436b6b750a7fbda
Tigasuloo <117059462+Tigasuloo@users.noreply.github.com>
2023-03-30 00:48:01 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const { Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions, MessageManager, Embed, Collection } = require(`discord.js`);
+const fs = require('fs');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] }); 
+
+client.commands = new Collection();
+
+require('dotenv').config();
+
+const functions = fs.readdirSync("./src/functions").filter(file => file.endsWith(".js"));
+const eventFiles = fs.readdirSync("./src/events").filter(file => file.endsWith(".js"));
+const commandFolders = fs.readdirSync("./src/commands");
+
+const activities = [
+    'Underdeveloping',
+    'XIII Servers',
+    '𝟏𝟑Ҝ Bot v.2',
+  ];
+
+  setInterval (() => {
+    const status = activities[Math.floor(Math.random() * activities.length)];
+    client.user.setActivity({name: status});
+  }, 60000);
+
+(async () => {
+    for (file of functions) {
+        require(`./functions/${file}`)(client);
+    }
+    client.handleEvents(eventFiles, "./src/events");
+    client.handleCommands(commandFolders, "./src/commands");
+    client.login(process.env.token)
+})();
+
